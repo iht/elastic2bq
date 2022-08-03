8119d0e0bb9977d25a408d0bc3a6287704de191f
Israel Herraiz <ihr@google.com>
2022-08-03 20:47:41 +0200

    WIP

--- src/main/java/dev/herraiz/beam/pipelines/Elastic2BQ.java
+++ src/main/java/dev/herraiz/beam/pipelines/Elastic2BQ.java
@@ -1,17 +1,21 @@
 package dev.herraiz.beam.pipelines;
 
 import dev.herraiz.beam.options.Elastic2BQOptions;
+import dev.herraiz.beam.schemas.JsonSchemaParser;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO;
 import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO.ConnectionConfiguration;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.JsonToRow;
+import org.apache.beam.sdk.transforms.JsonToRow.ParseResult;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
 
 public class Elastic2BQ {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Elastic2BQOptions options = PipelineOptionsFactory.fromArgs(args).as(Elastic2BQOptions.class);
 
     Pipeline p = Pipeline.create(options);
@@ -19,8 +23,7 @@ public class Elastic2BQ {
     String[] host = {options.getElasticHost()};
 
     String schemaStr = options.getSchema();
-
-
+    Schema schema = JsonSchemaParser.bqJson2BeamSchema(schemaStr);
 
     PCollection<String> jsonStrings = p.apply("Read from Elastic",
         ElasticsearchIO.read().withConnectionConfiguration(
@@ -30,6 +33,14 @@ public class Elastic2BQ {
                 options.getElasticType())
         ));
 
-    //jsonStrings.apply(JsonToRow.withExceptionReporting());
+    ParseResult parsedJson = jsonStrings.apply("Parse JSON",
+        JsonToRow.withExceptionReporting(schema));
+
+    // This will have our schema
+    PCollection<Row> correct = parsedJson.getResults();
+    // And the failures will have ERROR_ROW_SCHEMA
+    PCollection<Row> failed = parsedJson.getFailedToParseLines();
+
+    //correct.apply("Write corret to BigQuery", BigQueryIO.write())
   }
 }
