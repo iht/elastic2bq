fd922aa84fd8a258f454085f3e9bf4f22f0da738
Israel Herraiz <ihr@google.com>
2022-08-03 12:32:29 +0200

    WIP

--- src/main/java/dev/herraiz/beam/schemas/JsonSchemaParser.java
+++ src/main/java/dev/herraiz/beam/schemas/JsonSchemaParser.java
@@ -3,24 +3,28 @@ package dev.herraiz.beam.schemas;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.Schema.Field;
+import org.apache.beam.sdk.schemas.Schema.FieldType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JsonSchemaParser {
 
-  private static final String ROOT_NODE_NAME = "schema";
-  private static final String FIELDS_NODE_NAME = "fields";
+  private static final String ROOT_NODE_PATH = "schema";
+  private static final String FIELDS_NODE_PATH = "fields";
   private static final String REPEATED_MODE = "REPEATED";
+
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JsonSchemaParser.class);
 
-  private final Map<String, JsonNode> jsonFieldsMap;
+  private final Map<String, Field> beamFields = new HashMap<>();
 
   public JsonSchemaParser(String schemaAsString) {
-
-    this.jsonFieldsMap = new HashMap<>();
     ObjectMapper mapper = new ObjectMapper();
     JsonNode root = null;
     try {
@@ -30,18 +34,62 @@ public class JsonSchemaParser {
       LOGGER.error("Unable to parse the JSON schema provided. Make sure it is a BQ-style schema.");
       System.exit(1);
     }
-    JsonNode rootNode = root.path(ROOT_NODE_NAME);
+    JsonNode rootNode = root.path(ROOT_NODE_PATH);
   }
 
-  private static void parseSchema(JsonNode rootNode) {
-    for (JsonNode childField : rootNode.path(FIELDS_NODE_NAME)) {
-      if (childField.path("mode").asText().equals("REPEATED")) {
+  private Schema parseSchema(JsonNode rootNode) {
+    List<Field> beamFields = new ArrayList<>();
+    for (JsonNode childNode : rootNode.path(FIELDS_NODE_PATH)) {
+      boolean isRepated = childNode.path("mode").asText().equals(REPEATED_MODE);
 
+      String name = childNode.path("name").asText();
+      String type = childNode.path("type").asText().toUpperCase();
+
+      FieldType beamType;
+
+      if (type.equals("RECORD")) {
+        Schema recordSchema = parseSchema(childNode);
+        beamType = FieldType.row(recordSchema);
+      } else {
+        beamType = string2FieldType(type);
       }
+
+      Field field;
+      if (isRepated) {
+        field = Field.of(name, FieldType.array(beamType));
+      } else {
+        field = Field.of(name, beamType);
+      }
+
+      beamFields.add(field);
     }
+
+    Schema schema = Schema.of(beamFields.toArray(new Field[0]));
+
+    return schema;
   }
 
-  public Map<String, JsonNode> getJsonFieldsMap() {
-    return jsonFieldsMap;
+  private FieldType string2FieldType(String type) {
+    FieldType beamType = null;
+
+    if (type.equals("STRING")) {
+      beamType = FieldType.STRING;
+    } else if (type.equals("BYTES")) {
+      beamType = FieldType.BYTES;
+    } else if (type.equals("NUMERIC") || type.equals("BIGNUMERIC")) {
+      beamType = FieldType.DECIMAL;
+    } else if (type.equals("BOOL")) {
+      beamType = FieldType.BOOLEAN;
+    } else if (type.equals("DATETIME")) {
+      beamType = FieldType.DATETIME;
+    } else if (type.equals("INT64") || type.equals("INT")) {
+      beamType = FieldType.INT64;
+    } else if (type.equals("FLOAT64")) {
+      beamType = FieldType.DOUBLE;
+    }
+
+    assert beamType != null;
+    return beamType;
   }
+
 }
