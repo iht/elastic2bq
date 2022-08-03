7895ecec63097664653ee8cfe5a2a6dec528c1d6
Israel Herraiz <ihr@google.com>
2022-08-03 21:37:01 +0200

    WIP

--- src/main/java/dev/herraiz/beam/options/Elastic2BQOptions.java
+++ src/main/java/dev/herraiz/beam/options/Elastic2BQOptions.java
@@ -2,39 +2,64 @@ package dev.herraiz.beam.options;
 
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation.Required;
 
 public interface Elastic2BQOptions extends PipelineOptions {
 
+  @Required
   @Description("Hostname of the ElasticSearch server, including port")
   String getElasticHost();
 
   void setElasticHost(String s);
 
+  @Required
   @Description("ElasticSearch index to copy to BigQuery")
   String getElasticIndex();
 
   void setElasticIndex(String s);
 
+  @Required
   @Description("ELasticSearch type to copy to BigQuery")
   String getElasticType();
 
   void setElasticType(String s);
 
+  @Required
   @Description("BigQuery project")
   String getBigQueryProject();
 
   void setBigQueryProject(String s);
 
-  @Description("BigQuery dataset -- it must exist previously")
+  @Required
+  @Description("BigQuery dataset for the main table")
   String getBigQueryDataset();
 
   void setBigQueryDataset(String s);
 
-  @Description("BigQuery table")
+  @Required
+  @Description("BigQuery destination table name")
   String getBigQueryTable();
 
   void setBigQueryTable(String s);
 
+  @Description("BigQuery project for the errors table. Optional, if not passed the same project will be used for errors")
+  String getBigQueryErrorsProject();
+
+  void setBigQueryErrorsProject(String s);
+
+
+  @Description("BigQuery dataset for the errors table. Optional, if not passed the same dataset will be used for errors")
+  String getBigQueryErrorsDataset();
+
+  void setBigQueryErrorDataset(String s);
+
+  @Required
+  @Description("BigQuery table name for the parsing errors")
+  String getBigQueryErrorsTable();
+
+  void setBigQueryErrorsTable(String s);
+
+  @Required
   @Description("Schema of the type in Elastic, it is the same schema used for BigQuery")
   String getSchema();
 
--- src/main/java/dev/herraiz/beam/pipelines/Elastic2BQ.java
+++ src/main/java/dev/herraiz/beam/pipelines/Elastic2BQ.java
@@ -1,11 +1,16 @@
 package dev.herraiz.beam.pipelines;
 
+import com.google.api.services.bigquery.model.TableReference;
 import dev.herraiz.beam.options.Elastic2BQOptions;
 import dev.herraiz.beam.schemas.JsonSchemaParser;
+import java.util.Optional;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO;
 import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO.ConnectionConfiguration;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.JsonToRow;
@@ -16,15 +21,41 @@ import org.apache.beam.sdk.values.Row;
 public class Elastic2BQ {
 
   public static void main(String[] args) throws Exception {
-    Elastic2BQOptions options = PipelineOptionsFactory.fromArgs(args).as(Elastic2BQOptions.class);
+    // Parse and set options
+    Elastic2BQOptions options = PipelineOptionsFactory.fromArgs(args)
+        .withValidation()
+        .as(Elastic2BQOptions.class);
 
-    Pipeline p = Pipeline.create(options);
+    // BigQuery options
+    String bigQueryProject = options.getBigQueryProject();
+    String bigQueryDataset = options.getBigQueryDataset();
+    String bigQueryTable = options.getBigQueryTable();
+    String errorsProject = Optional.ofNullable(options.getBigQueryErrorsProject())
+        .orElse(bigQueryProject);
+    String errorsDataset = Optional.ofNullable(options.getBigQueryErrorsDataset())
+        .orElse(bigQueryDataset);
+    String errorsTable = options.getBigQueryErrorsTable();
 
+    TableReference correctTableRef = new TableReference()
+        .setProjectId(bigQueryProject)
+        .setDatasetId(bigQueryDataset)
+        .setTableId(bigQueryTable);
+
+    TableReference errorsTableRef = new TableReference()
+        .setProjectId(errorsProject)
+        .setDatasetId(errorsDataset)
+        .setTableId(errorsTable);
+
+    // Elastic options
     String[] host = {options.getElasticHost()};
 
+    // Schema options
     String schemaStr = options.getSchema();
     Schema schema = JsonSchemaParser.bqJson2BeamSchema(schemaStr);
 
+    Pipeline p = Pipeline.create(options);
+
+    // Read data from Elastic as JSON strings
     PCollection<String> jsonStrings = p.apply("Read from Elastic",
         ElasticsearchIO.read().withConnectionConfiguration(
             ConnectionConfiguration.create(
@@ -33,6 +64,7 @@ public class Elastic2BQ {
                 options.getElasticType())
         ));
 
+    // Parse JSON strings to Beam Row
     ParseResult parsedJson = jsonStrings.apply("Parse JSON",
         JsonToRow.withExceptionReporting(schema));
 
@@ -41,6 +73,20 @@ public class Elastic2BQ {
     // And the failures will have ERROR_ROW_SCHEMA
     PCollection<Row> failed = parsedJson.getFailedToParseLines();
 
-    //correct.apply("Write corret to BigQuery", BigQueryIO.write())
+    // Write to BigQuery
+    correct.apply("Write correct to BQ", bqWriteForTable(correctTableRef));
+    failed.apply("Write failures to BQ", bqWriteForTable(errorsTableRef));
   }
+
+  private static Write<Row> bqWriteForTable(TableReference table) {
+    return BigQueryIO.<Row>write()
+        .to(table)
+        .optimizedWrites()
+        .withAutoSharding()
+        .withCreateDisposition(CreateDisposition.CREATE_IF_NEEDED)
+        .withWriteDisposition(WriteDisposition.WRITE_APPEND)
+        .withMethod(Write.Method.FILE_LOADS)
+        .useBeamSchema();
+  }
+
 }
