8c47a3e51a4d01562b57bc058dd0ba8301ea0d8d
Israel Herraiz <ihr@google.com>
2022-08-28 22:35:08 +0200

    Add copyright headers

--- src/main/java/dev/herraiz/beam/parser/Json2RowWithSanitization.java
+++ src/main/java/dev/herraiz/beam/parser/Json2RowWithSanitization.java
@@ -1,33 +1,20 @@
-package dev.herraiz.beam.parser;
+/*
+ * Copyright 2022 Israel Herraiz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// import static
-// org.apache.beam.sdk.transforms.JsonToRow.JsonToRowWithErrFn.ERROR_ROW_WITH_ERR_MSG_SCHEMA;
-//
-// import com.fasterxml.jackson.core.JsonProcessingException;
-// import com.fasterxml.jackson.databind.JsonNode;
-// import com.fasterxml.jackson.databind.ObjectMapper;
-// import com.google.auto.value.AutoValue;
-// import com.google.common.collect.ImmutableMap;
-// import dev.herraiz.beam.parser.Json2Row.JsonParseResult;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.stream.Stream;
-// import java.util.stream.StreamSupport;
-// import org.apache.beam.sdk.Pipeline;
-// import org.apache.beam.sdk.schemas.Schema;
-// import org.apache.beam.sdk.schemas.Schema.Field;
-// import org.apache.beam.sdk.schemas.Schema.FieldType;
-// import org.apache.beam.sdk.transforms.DoFn;
-// import org.apache.beam.sdk.transforms.PTransform;
-// import org.apache.beam.sdk.transforms.ParDo;
-// import org.apache.beam.sdk.values.PCollection;
-// import org.apache.beam.sdk.values.PCollectionTuple;
-// import org.apache.beam.sdk.values.PInput;
-// import org.apache.beam.sdk.values.POutput;
-// import org.apache.beam.sdk.values.PValue;
-// import org.apache.beam.sdk.values.Row;
-// import org.apache.beam.sdk.values.TupleTag;
-// import org.apache.beam.sdk.values.TupleTagList;
+package dev.herraiz.beam.parser;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -117,8 +104,6 @@ public class Json2RowWithSanitization extends PTransform<PCollection<String>, Pa
           List<JsonNode> objects;
           if (wrappedType.getTypeName().isCompositeType() || wrappedType.getTypeName().isCollectionType()) {
             // If the collection type is a Row/Struct, or it is a list of lists
-//            FieldType collectionType =
-//                Objects.requireNonNull(childField.getType().getCollectionElementType());
             Schema collectionSchema = Objects.requireNonNull(wrappedType.getRowSchema());
             objects =
                     stream.map(n -> sanitizeNode(n, collectionSchema)).collect(Collectors.toList());
@@ -153,168 +138,3 @@ public class Json2RowWithSanitization extends PTransform<PCollection<String>, Pa
     }
   }
 }
-
-// public class Json2Row extends PTransform<PCollection<String>,
-// JsonParseResult> {
-//
-// final Schema schema;
-//
-// public Json2Row(Schema schema) {
-// this.schema = schema;
-// }
-//
-// @Override
-// public JsonParseResult expand(PCollection<String> input) {
-//
-// PCollectionTuple parsed =
-// input.apply(
-// "Parse JSON to Row",
-// ParDo.of(new Json2RowDoFn(schema))
-// .withOutputTags(
-// Json2RowDoFn.PARSED_ROWS, TupleTagList.of(Json2RowDoFn.FAILED_ROWS)));
-//
-// PCollection<Row> parsedRows = parsed.get(Json2RowDoFn.PARSED_ROWS);
-// PCollection<Row> failedRows = parsed.get(Json2RowDoFn.FAILED_ROWS);
-//
-// return JsonParseResult.builder()
-// .schema(schema)
-// .parsedRows(parsedRows)
-// .failedRows(failedRows)
-// .callingPipeline(input.getPipeline())
-// .build();
-// }
-//
-// @AutoValue
-// public abstract static class JsonParseResult implements POutput {
-// public JsonParseResult() {}
-//
-// public abstract PCollection<Row> parsedRows();
-//
-// public abstract PCollection<Row> failedRows();
-//
-// abstract Schema schema();
-//
-// abstract Pipeline callingPipeline();
-//
-// public Pipeline getPipeline() {
-// return this.callingPipeline();
-// }
-//
-// @Override
-// public void finishSpecifyingOutput(
-// String transformName, PInput input, PTransform<?, ?> transform) {}
-//
-// public Map<TupleTag<?>, PValue> expand() {
-// return ImmutableMap.of(
-// Json2RowDoFn.PARSED_ROWS, this.parsedRows(),
-// Json2RowDoFn.FAILED_ROWS, this.failedRows());
-// }
-//
-// static JsonParseResult.Builder builder() {
-// return new AutoValue_Json2Row_JsonParseResult.Builder();
-// }
-//
-// @AutoValue.Builder
-// public abstract static class Builder {
-// public Builder() {}
-//
-// abstract JsonParseResult.Builder parsedRows(PCollection<Row> parsed);
-//
-// abstract JsonParseResult.Builder failedRows(PCollection<Row> failed);
-//
-// abstract JsonParseResult.Builder callingPipeline(Pipeline p);
-//
-// abstract JsonParseResult.Builder schema(Schema s);
-//
-// abstract JsonParseResult build();
-// }
-// }
-//
-// private static class Json2RowDoFn extends DoFn<String, Row> {
-//
-// static final TupleTag<Row> PARSED_ROWS = new TupleTag<Row>() {};
-// static final TupleTag<Row> FAILED_ROWS = new TupleTag<Row>() {};
-//
-// final Schema schema;
-// ObjectMapper objectMapper;
-//
-// public Json2RowDoFn(Schema schema) {
-// this.schema = schema;
-// }
-//
-// @Setup
-// public void setup() {
-// objectMapper = new ObjectMapper();
-// }
-//
-// @ProcessElement
-// public void process(@Element String jsonStr, MultiOutputReceiver receiver) {
-// try {
-// JsonNode node = objectMapper.readTree(jsonStr);
-// Row row = parseFields(node, this.schema);
-// receiver.getRowReceiver(PARSED_ROWS).output(row);
-// } catch (JsonProcessingException e) {
-// Row row =
-// Row.withSchema(ERROR_ROW_WITH_ERR_MSG_SCHEMA)
-// .withFieldValue("line", jsonStr)
-// .withFieldValue("err", e.getMessage())
-// .build();
-// receiver.getRowReceiver(FAILED_ROWS).output(row);
-// }
-// }
-//
-// private Row parseFields(JsonNode node, Schema schema) {
-// List<Field> fields = schema.getFields();
-//
-// Row.Builder rowBuilder = Row.withSchema(schema);
-//
-// for (Field f : fields) {
-// String fieldName = f.getName();
-// JsonNode childNode = node.get(fieldName);
-// Field childField = schema.getField(fieldName);
-// if (f.getType().getTypeName().isCompositeType()) {
-// // Recursive iteration if this is a Row
-// Schema childSchema = childField.getType().getRowSchema();
-// assert childSchema != null;
-// rowBuilder.withFieldValue(fieldName, parseFields(childNode, childSchema));
-// } else if (f.getType().getTypeName().isCollectionType()) {
-// // Add array if this is a collection
-// FieldType wrappedType = f.getType().getCollectionElementType();
-// assert childNode.isArray();
-// Stream<JsonNode> stream = StreamSupport.stream(childNode.spliterator(),
-// false);
-// List<Object> objects = stream.map(n -> parseField(n, wrappedType)).toList();
-// rowBuilder.withFieldValue(fieldName, objects);
-// } else {
-// // Single type field
-// rowBuilder.withFieldValue(fieldName, parseField(childNode,
-// childField.getType()));
-// }
-// }
-//
-// return rowBuilder.build();
-// }
-//
-// @SuppressWarnings("unchecked")
-// private static <T> T parseField(JsonNode node, FieldType type) {
-// if (type == FieldType.STRING) {
-// return (T) node.asText();
-// } else if (type == FieldType.DOUBLE || type == FieldType.FLOAT) {
-// return (T) Double.valueOf(node.asDouble());
-// } else if (type == FieldType.INT64 || type == FieldType.INT32 || type ==
-// FieldType.INT16) {
-// return (T) Integer.valueOf(node.asInt());
-// } else if (type == FieldType.BYTES) {
-// return (T) node.asText(); // FIXME
-// } else if (type == FieldType.DECIMAL) {
-// return (T) node.asText(); // FIXME
-// } else if (type == FieldType.BOOLEAN) {
-// return (T) Boolean.valueOf(node.asBoolean()); // FIXME
-// } else if (type == FieldType.DATETIME) {
-// return (T) node.asText(); // FIXME
-// }
-//
-// return (T) node.asText(); // FIXME
-// }
-// }
-// }
